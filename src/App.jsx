a4e38a140021a7c1b17888dde98b118b46763045
gokul <gokulchandan24@gmail.com>
2024-11-25 00:43:22 +0530

    public folder added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import NavBar from "./components/Navbar";
+
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <main className="relative min-h-screen w-screen overflow-x-hidden">
+        <NavBar />
+      </main>
     </>
   );
 }
